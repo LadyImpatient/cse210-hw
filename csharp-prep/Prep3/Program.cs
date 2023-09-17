b70e8b96b16a1d33b7440a10822c1f7795b1f62e
LadyImpatient <aliciapuuwai@gmail.com>
2023-09-16 21:33:20 -0500

    Prep3 assignment completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //random number
+        Random randomGenerator = new Random();
+        int x = randomGenerator.Next(1, 101);
+
+        int y = -1;
+
+        while (y != x)
+            {
+                //ask for guess
+                Console.WriteLine("What is your guess? ");
+                y = int.Parse(Console.ReadLine());
+                
+                //if statements
+                if (y > x)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                else if (y < x)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                else    
+                    {
+                        Console.WriteLine("You guessed it!");
+                    }
+            }
     }
 }
