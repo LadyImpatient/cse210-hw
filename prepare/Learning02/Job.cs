9d68de8ae0565b043a3861b7364ebc91111d29d3
LadyImpatient <aliciapuuwai@gmail.com>
2023-09-17 22:22:47 -0500

    patch attempt to fix errors. one left

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,16 +2,14 @@
 using System;
 
 public class Job
-{   
-    //variable to store job information
-    public string _company;
+{
     public string _jobTitle;
+    public string _company;
     public int _startYear;
     public int _endYear;
 
-    //print job info
-    public void DisplayJobInfo()
+    public void Display()
     {
-        Console.WriteLine($"{_jobTitle}, {_company} {_startYear}-{_endYear}");
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -3,16 +3,17 @@ using System;
 public class Resume
 {
     public string _name;
+
     public List<Job> _jobs = new List<Job>();
 
-    public void DisplayJobList()
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
         {
-            Console.WriteLine($"Name: {_name}");
-            Console.WriteLine($"Jobs: \n {_jobs}");
-    
-            foreach (Job job in _jobs)
-                {
-                    job.DisplayJobList();
-                }
-        }    
+            job.Display();
+        }
+    }
 }
