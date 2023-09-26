4fa528a229b31974e878301017e55d00b8d0bffb
LadyImpatient <aliciapuuwai@gmail.com>
2023-09-25 21:16:21 -0500

    class fraction begun, private variables created

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        class Fraction
+            {
+                private int top;
+                private int bottom;
+//no parameters
+                public Fraction()
+                {
+                    top = 1;
+                    bottom = 1;
+                }
+//top
+                public Fraction(int top)
+                {
+                    f.top = Top;
+                    bottom = 1;
+                }
+
+            }
     }
 }
