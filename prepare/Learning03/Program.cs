283eeab40a0ca3292637897894f17a4b11ec5232
LadyImpatient <aliciapuuwai@gmail.com>
2023-09-26 19:39:44 -0500

    final code - functioning

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -43,6 +43,18 @@ class Program
         Fraction f1 = new Fraction();
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimal());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimal());
+
+        Fraction f3 = new Fraction(3,4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimal());
+
+        Fraction f4 = new Fraction(1,3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimal());
     }
 }
 
