124dffc76f268c596a647190588875acafd57374
LadyImpatient <aliciapuuwai@gmail.com>
2023-10-07 20:51:45 -0500

    breathing and reflection update

--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -7,6 +7,26 @@ class BreathingActivity : Activity
     public override void Start()
     {
         base.Start();
-        Console.WriteLine("Description: This activity will help you relax by guiding you through breathing in and out slowly. Clear your mind and focus on your breathing.");
+        Console.WriteLine("Description: This activity will help you relax by guiding you through breathing in and out slowly. Clear your mind and focus on your breathing.");     
+        
+        int durationInSeconds = duration;
+        while (durationInSeconds > 0)
+        {
+            Console.WriteLine("Breathe in...");
+            Wait(3);
+
+            Console.WriteLine("Breathe out...");
+            Wait(3);
+
+            durationInSeconds -= 6;
+        }
+
+        Console.WriteLine("Breathing activity completed.");
+        Console.ReadLine();
+    }
+    private void Wait(int seconds)
+    {
+        DateTime start = DateTime.Now;
+        while ((DateTime.Now - start).TotalSeconds < seconds) { }
     }
 }
--- prove/Develop04/Reflection.cs
+++ prove/Develop04/Reflection.cs
@@ -8,5 +8,42 @@ class ReflectionActivity : Activity
     {
         base.Start();
         Console.WriteLine("Description: This activity will help you reflect on times in your life when you have shown strength and resilience.");
+         string[] prompts = new string[]
+        {
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
+        };
+
+        string[] questions = new string[]
+        {
+            "Why was this experience meaningful to you?",
+            "Have you ever done anything like this before?",
+            "How did you get started?",
+            "How did you feel when it was complete?",
+            "What made this time different than other times when you were not as successful?",
+            "What is your favorite thing about this experience?",
+            "What could you learn from this experience that applies to other situations?",
+            "What did you learn about yourself through this experience?",
+            "How can you keep this experience in mind in the future?"
+        };
+
+        Random random = new Random();
+        foreach (string prompt in prompts)
+        {
+            Console.WriteLine(prompt);
+            Console.ReadLine();
+            foreach (string question in questions)
+            {
+                Console.WriteLine(question);
+                // Pause for reflection, you can use Console.ReadLine or another method if desired
+                Console.WriteLine("Press Enter to continue to the next question.");
+                Console.ReadLine();
+            }
+        }
+
+        Console.WriteLine("Reflection activity completed.");
+        Console.ReadLine();
     }
 }
