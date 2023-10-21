fc1f09d2a613fe8a0dfa27482c955256e39c632e
LadyImpatient <aliciapuuwai@gmail.com>
2023-10-20 23:01:23 -0500

    new class to make total 8 classes

---
+++ final/FinalProject/PracticeTest.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class PracticeTest
+{
+    private List<Organelle> organelles;
+
+    public PracticeTest()
+    {
+        organelles = new List<Organelle>();
+    }
+
+    public void AddOrganelle(Organelle organelle)
+    {
+        organelles.Add(organelle);
+    }
+
+    public void StartTest()
+    {
+       
+    }
+
+    private List<string> GenerateOptions()
+    {
+
+    }
+
+    private void Shuffle<T>(IList<T> list)
+    {
+
+    }
+}
